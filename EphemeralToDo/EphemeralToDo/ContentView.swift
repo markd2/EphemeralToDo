a777bfee02309b85033a94c3453e7dacf65b757f
MarkD <markd@borkware.com>
2023-12-30 15:10:23 -0500

    Infra for changing views

--- EphemeralToDo/EphemeralToDo/ContentView.swift
+++ EphemeralToDo/EphemeralToDo/ContentView.swift
@@ -2,9 +2,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State var entries: [Entry] = []
+    @State var screen: Screen = .entry
+
+    enum Screen {
+        case entry
+        case run
+    }
 
     var body: some View {
-        EntryView(entries: $entries)
+        switch screen {
+            case .entry: 
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button("Done") {
+                            print("Splunge")
+                            screen = .run
+                        }.padding([.trailing])
+                    }
+                    EntryView(entries: $entries)
+                }
+            case .run:
+                RunView()
+        }
     }
 }
 
