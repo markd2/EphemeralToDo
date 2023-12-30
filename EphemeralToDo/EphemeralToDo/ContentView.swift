449e3df5ed4203320d844c26a9a89f3227c96cda
MarkD <markd@borkware.com>
2023-12-30 13:21:21 -0500

    new template

---
+++ EphemeralToDo/EphemeralToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EphemeralToDo
+//
+//  Created by Mark Dalrymple on 12/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EphemeralToDo/EphemeralToDo/EphemeralToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EphemeralToDoApp.swift
+//  EphemeralToDo
+//
+//  Created by Mark Dalrymple on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct EphemeralToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
