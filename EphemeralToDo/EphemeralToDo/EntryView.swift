af03a11964b89367580eebc2ad12f0459d670119
MarkD <markd@borkware.com>
2023-12-30 14:49:23 -0500

    Yay can accumulate into an array.

--- EphemeralToDo/EphemeralToDo/EntryView.swift
+++ EphemeralToDo/EphemeralToDo/EntryView.swift
@@ -1,11 +1,44 @@
 import SwiftUI
 
 struct EntryView: View {
+    @State private var thing: String = ""
+    @State var entries: [Entry] = []
+    @FocusState private var focused: Bool
+
     var body: some View {
-        Text("Greeble")
+        VStack {
+            TextField("Some Thing", text: $thing)
+              .padding()
+
+              .onSubmit {
+                  makeNewThing(from: thing)
+                  thing = ""
+                  focused = true
+              }
+            .focused($focused)
+              .textInputAutocapitalization(.never)
+              .disableAutocorrection(true)
+              .border(.secondary)
+            List(entries) { entry in
+                Text(entry.thing)
+            }
+            .listStyle(.plain)
+        }
+          .padding()
+        .onAppear {
+            focused = true
+        }
+    }
+
+    func makeNewThing(from thing: String) {
+        print("MAKE NEW THING \(thing)")
+        let entry = Entry(thing: thing)
+        entries.append(entry)
     }
 }
 
 #Preview {
-    EntryView()
+    let entries = ["greeble", "bork", "splunge"]
+      .map(Entry.init)
+    return EntryView(entries: entries)
 }
